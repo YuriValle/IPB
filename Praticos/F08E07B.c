db056750f059173b58a106698b84e5329326d575
YuriValle <YuriValle@YuriValle>
2016-12-02 15:05:42 +0000

    #ADD
    Ficha08

---
+++ Praticos/F08E07B.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main (void) {
+	printf("Hello World!\n\tEste e o meu primeiro programa!\n\tE funciona mesmo!!!\n");
+}
---
+++ Praticos/F08E08.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void) {
+	int idade;
+	printf("Digite sua idade. :D!\n");
+	scanf("%d", &idade);
+	printf("Sua idade em decimal: %d\n", idade);
+	printf("Sua idade em octal: %o\n", idade);
+	printf("sua idade em hexadecimal: %x\n", idade);
+}
