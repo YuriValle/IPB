fd7999bd3b2945e9088f0401f580f16a853412a8
YuriValle <YuriValle@YuriValle>
2016-12-02 15:05:53 +0000

    #ADD
    Sessão 02

---
+++ Suporte/S02E02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main () {
+	char letra;
+	printf("Digite uma letra: \n");
+	scanf("%c", &letra);
+	printf("        %c   %c   %c   %c   %c%c%c%c     %c%c%c\n", letra,letra,letra,letra,letra,letra,letra,letra,letra,letra,letra);
+	printf("        %c   %c   %c   %c   %c   %c     %c\n", letra,letra,letra,letra,letra,letra,letra);
+	printf("         %c %c    %c   %c   %c   %c     %c\n", letra,letra,letra,letra,letra,letra,letra);
+	printf("         %c %c    %c   %c   %c  %c      %c\n", letra,letra,letra,letra,letra,letra,letra);
+	printf("          %c     %c   %c   %c%c%c%c      %c\n", letra,letra,letra,letra,letra,letra,letra,letra);
+	printf("          %c     %c   %c   %c %c       %c\n", letra,letra,letra,letra,letra,letra);
+	printf("          %c     %c   %c   %c  %c      %c\n", letra,letra,letra,letra,letra,letra);
+	printf("          %c     %c%c%c%c%c   %c   %c    %c%c%c\n", letra,letra,letra,letra,letra,letra,letra,letra,letra,letra,letra);
+	printf("\n");
+	printf("\n");
+	printf("   %c   %c    %c%c%c    %c       %c       %c%c%c%c%c\n", letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra);
+	printf("   %c   %c   %c   %c   %c       %c       %c\n", letra, letra, letra, letra, letra, letra, letra);
+	printf("   %c   %c   %c   %c   %c       %c       %c\n", letra, letra, letra, letra, letra, letra, letra);
+	printf("    %c %c    %c   %c   %c       %c       %c%c%c%c%c\n", letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra);
+	printf("    %c %c    %c   %c   %c       %c       %c\n", letra, letra, letra, letra, letra, letra, letra);
+	printf("    %c %c    %c%c%c%c%c   %c       %c       %c\n", letra, letra, letra, letra, letra, letra, letra, letra, letra, letra);
+	printf("     %c     %c   %c   %c       %c       %c\n", letra, letra, letra, letra, letra, letra);
+	printf("     %c     %c   %c   %c%c%c%c%c   %c%c%c%c%c   %c%c%c%c%c\n", letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra, letra);
+}
