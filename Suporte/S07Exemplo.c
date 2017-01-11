42d40382ae1ce9b937714858e135f697b8e3374a
Yuri Valle <Yuri.Valle@Gmail.Com>
2017-01-11 01:21:44 +0000

    #ADD
    Exemplo de array de strings
    Exemplo de 'tamanho de elementos em um array'

---
+++ Suporte/S07Exemplo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int i;
+	char nomes[][20] = 	{"Bruce",	"Lex",		"Peter"};
+	char senhas[][10] = {"Wayne",	"Luthor",	"Parker"};
+	float saldos[] = 	{999.99,	-509.92,	250.12};
+	int arraySize = sizeof(nomes) / sizeof((nomes)[0]);
+	for(i=0;i<arraySize;i++){
+		printf("Conta: %s\n", nomes[i]);
+		printf("Senha: %s\n", senhas[i]);
+		printf("Saldo: %.2f\n\n", saldos[i]);
+	}
+}
