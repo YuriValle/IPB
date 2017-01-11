9508e08ad29e645c26776ee5ff7164b901fd6e63
Yuri Valle <Yuri.Valle@Gmail.Com>
2017-01-11 03:12:07 +0000

    #BUGFIX
    Consertando rebase do git

--- Suporte/S08E02.c
+++ Suporte/S08E02.c
@@ -2,13 +2,8 @@
 
 int main(){
 	int i;
-	char nomes[][20] = {"Bruce","Lex","Peter"};
-	char senhas[][10] = {"Wayne","Luthor","Parker"};
-	float saldos[] = {999.99,-509.92,250.12};
-	int arraySize = sizeof(nomes) / sizeof((nomes)[0]);
-	for(i=0;i<arraySize;i++){
-		printf("Conta: %s\n", nomes[i]);
-		printf("Senha: %s\n", senhas[i]);
-		printf("Saldo: %.2f\n\n", saldos[i]);
+	char nomes[][20] = {"Bruce", "Lex", "Peter"};
+	for(i=0;i<3;i++){
+		printf("Nome na posicao %d: %s\n", i, nomes[i]);
 	}
 }
