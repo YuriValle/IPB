db17d91550360b4e4309402ff3f4e51341dbc3fe
Yuri Valle <yuri.valle@gmail.com>
2017-01-11 01:23:36 +0000

    #Edit
    Correção de espaços no código

--- Suporte/S07Exemplo.c
+++ Suporte/S07Exemplo.c
@@ -2,9 +2,9 @@
 
 int main(){
 	int i;
-	char nomes[][20] = 	{"Bruce",	"Lex",		"Peter"};
-	char senhas[][10] = {"Wayne",	"Luthor",	"Parker"};
-	float saldos[] = 	{999.99,	-509.92,	250.12};
+	char nomes[][20] = {"Bruce","Lex","Peter"};
+	char senhas[][10] = {"Wayne","Luthor","Parker"};
+	float saldos[] = {999.99,-509.92,250.12};
 	int arraySize = sizeof(nomes) / sizeof((nomes)[0]);
 	for(i=0;i<arraySize;i++){
 		printf("Conta: %s\n", nomes[i]);
